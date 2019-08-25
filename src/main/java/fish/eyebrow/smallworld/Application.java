e275667bad2b145dd4dcf6e95b2f585c354d259a
Alexander Johnston <Aliics@hotmail.com>
2019-08-25 21:43:59 +1200

    Setup LibGDX project

---
+++ src/main/java/fish/eyebrow/smallworld/Application.java
@@ -0,0 +1,11 @@
+package fish.eyebrow.smallworld;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+    }
+}
